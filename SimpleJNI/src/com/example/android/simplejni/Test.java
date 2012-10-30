f3a91524c50f3225d4ca3aec5d01b15cc767cdae
jieum1214 <jieum1214@gmail.com>
2012-10-30 14:16:55 +0900

    SimpleJNI_B20 커밋..

---
+++ SimpleJNI/src/com/example/android/simplejni/Test.java
@@ -0,0 +1,21 @@
+package com.example.android.simplejni;
+
+public class Test {
+
+	private String name;
+	private int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+}
